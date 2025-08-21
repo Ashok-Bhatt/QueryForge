753cd4070cf811f50abc372418c6bbf7f0e4073c
Ashok-Bhatt <ashokbhatt2048@gmail.com>
2025-08-21 22:59:56 +0530

    Add API Instructons in base route

--- server/app.js
+++ server/app.js
@@ -22,4 +22,47 @@ app.use(cors(corsOption));
 app.use("/api/v1/qna", qnaRouter);
 app.use("/api/v1/bot", botRouter);
 
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "Welcome to the QnA Bot API",
+        apiEndpoints: [
+            {
+                endpoint: "https://queryforge-8cgm.onrender.com/api/v1/qna",
+                method: "POST",
+                description: "Create a new QnA bot",
+                body: {
+                    "description": "string",
+                    "attachments": "files (optional, can be multiple files)"
+                },
+                returns: {
+                    "statusCode": "number",
+                    "data": {
+                        "description": "string",
+                        "uniqueCode": "string",
+                    },
+                    "message": "string",
+                    "success": "boolean"
+                }
+            },
+            {
+                endpoint: "https://queryforge-8cgm.onrender.com/api/v1/bot/ask",
+                method: "POST",
+                description: "Ask a question to the QnA bot",
+                body: {
+                    "qnaCode": "string (unique code of the QnA bot generated during creation)",
+                    "question": "string"
+                },
+                returns: {
+                    "statusCode": "number",
+                    "data": {
+                        "response": "string (response to the question asked)"
+                    },
+                    "message": "string",
+                    "success": "boolean"
+                }
+            }
+        ]
+    });
+});
+
 export {app};
